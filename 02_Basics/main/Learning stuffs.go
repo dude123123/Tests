17e5064a096f1a91b64cbe95c0e22f6b93f0e105
dude123123 <wuyuu8@gmail.com>
2017-03-05 18:29:33 -0500

    Updated some older tut files, uploaded some more files from lessons

---
+++ 02_Basics/main/Learning stuffs.go
@@ -0,0 +1,11 @@
+package main
+
+import	(
+	"fmt"
+	"github.com/dude123123/Tests/02_Basics/stringutil"
+)
+
+func main(){
+	fmt.Println(stringutil.Reverse("!oG olleH")) //from this package (stringutil, and use this command, Reverse, Capitalized = exportable, non-capital = not exportable)
+	fmt.Println(stringutil.MyName)
+}
